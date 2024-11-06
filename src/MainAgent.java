336949d7028f3467706900d7808eb2735011372f
Pirito10 <aaronriveiro@gmail.com>
2024-11-07 00:36:30 +0100

    added test code for main class

--- src/MainAgent.java
+++ src/MainAgent.java
@@ -1,1 +1,7 @@
-package src;
+public class MainAgent {
+    public static void main(String[] args) {
+        // Código de inicialización de tu agente
+        System.out.println("Ejecutando MainAgent...");
+        // Aquí puedes agregar la lógica para iniciar JADE y cargar tu agente
+    }
+}
