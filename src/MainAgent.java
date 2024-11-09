53807700100a8d2e49316f64a0dad3c2098b89a4
Pirito10 <aaronriveiro@gmail.com>
2024-11-09 02:54:59 +0100

    added methods to process inflation and stock value, added list to store payoffs, send round over messages

--- src/MainAgent.java
+++ src/MainAgent.java
@@ -1,4 +1,6 @@
+import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import jade.core.AID;
 import jade.core.Agent;
@@ -14,7 +16,7 @@ public class MainAgent extends Agent {
     // Variables para almacenar el número de jugadores, de rondas y el porcentaje de
     // comisión
     private int N;
-    private final int R = 2;
+    private final int R = 4;
     private final double F = 0.1;
 
     // Lista para almacenar los AID de los jugadores
@@ -25,6 +27,9 @@ public class MainAgent extends Agent {
             { { 2, 2 }, { 0, 4 } },
             { { 4, 0 }, { 0, 0 } }
     };
+
+    // Formateador para limitar a dos decimales
+    private static final DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void setup() {
@@ -99,9 +104,30 @@ public class MainAgent extends Agent {
                     System.out.println("[Main] Mensaje enviado a jugador con ID " + i + ": " + message);
                 }
 
+                // HashMap para almacenar el payoff acumulado de cada jugador
+                HashMap<AID, Double> totalPayoffs = new HashMap<>();
+                // Inicializamos el HashMap a cero
+                for (AID player : players) {
+                    totalPayoffs.put(player, 0.0);
+                }
+
+                // HashMap para almacenar los stocks de cada jugador
+                HashMap<AID, Double> totalStocks = new HashMap<>();
+                // Inicializamos el HashMap a cero
+                for (AID player : players) {
+                    totalStocks.put(player, 0.0);
+                }
+
                 // Iteramos sobre el número de rondas
                 for (int round = 1; round <= R; round++) {
                     System.out.println("\n[Main] Iniciando la ronda " + round + "...");
+
+                    // HashMap para acumular el payoff de cada jugador durante la ronda
+                    HashMap<AID, Integer> roundPayoffs = new HashMap<>();
+                    // Inicializamos el HashMap a cero
+                    for (AID player : players) {
+                        roundPayoffs.put(player, 0);
+                    }
 
                     // Iteramos sobre cada pareja de jugadores
                     for (int i = 0; i < players.size() - 1; i++) {
@@ -144,7 +170,15 @@ public class MainAgent extends Agent {
                             String action_player2 = reply_player2.getContent().split("#")[1];
 
                             // Procesamos el resultado
-                            int payoffs[] = getPayoffs(action_player1, action_player2);
+                            int[] payoffs = getPayoffs(action_player1, action_player2);
+
+                            // Actualizamos el payoff de la ronda de cada jugador
+                            roundPayoffs.put(player1, roundPayoffs.get(player1) + payoffs[0]);
+                            roundPayoffs.put(player2, roundPayoffs.get(player2) + payoffs[1]);
+
+                            // Actualizamos el payoff acumulado de cada jugador
+                            totalPayoffs.put(player1, totalPayoffs.get(player1) + payoffs[0]);
+                            totalPayoffs.put(player2, totalPayoffs.get(player2) + payoffs[1]);
 
                             // Construímos el mensaje de resultados
                             message = "Results#" + i + "," + j + "#" + action_player1 + "," + action_player2 + "#"
@@ -156,6 +190,27 @@ public class MainAgent extends Agent {
                             System.out.println(
                                     "[Main] Mensaje enviado a jugadores con ID " + i + "," + j + ": " + message);
                         }
+                    }
+
+                    // Recorremos la lista de jugadores
+                    for (int i = 0; i < players.size(); i++) {
+
+                        // Obtenemos el jugador
+                        AID player = players.get(i);
+                        // Obtenemos su payoff de la ronda
+                        int roundPayoff = roundPayoffs.get(player);
+                        // TODO aplicar el valor de inflación
+                        // Obtenemos su payoff acumulado
+                        double totalPayoff = totalPayoffs.get(player);
+                        // Obtenemos sus stocks
+                        double stocks = totalStocks.get(player);
+
+                        String message = "RoundOver#" + i + "#" + roundPayoff + "#" + totalPayoff + "#"
+                                + getInflationRate(round)
+                                + "#" + stocks + "#" + getIndexValue(round);
+
+                        // Enviamos el mensaje
+                        sendMessage(ACLMessage.REQUEST, player, message);
                     }
                 }
             }
@@ -194,5 +249,17 @@ public class MainAgent extends Agent {
 
         // Obtenemos los payoffs de la matriz
         return matrix[index_player1][index_player2];
+    }
+
+    // Método que calcula el valor del stock en función de la ronda
+    public double getIndexValue(int round) {
+        double indexValue = 100 * Math.log(round + 1) + 50;
+        return Double.parseDouble(df.format(indexValue));
+    }
+
+    // Método que calcula la tasa de inflación en función de la ronda
+    public double getInflationRate(int round) {
+        double inflationRate = 0.5 + 0.5 * Math.sin(round * Math.PI / 10);
+        return Double.parseDouble(df.format(inflationRate));
     }
 }
