04f3ce8aecf604e51f7a08f328423a15106a1d8a
Pirito10 <aaronriveiro@gmail.com>
2024-11-17 03:26:44 +0100

    added handling to remove players

--- src/MainAgent.java
+++ src/MainAgent.java
@@ -24,6 +24,7 @@ public class MainAgent extends Agent {
 
     // Lista para almacenar los jugadores
     private static ArrayList<Player> players = new ArrayList<>();
+    public final ArrayList<Player> playersToRemove = new ArrayList<>();
 
     // Matriz con los payoffs
     private static final int[][][] matrix = {
@@ -93,6 +94,7 @@ public class MainAgent extends Agent {
                             addPlayerToTable(player);
                             log("\t- " + agentName);
                         }
+
                         // Si hay uno o cero jugadores, no se continúa
                     } else if (result.length == 1) {
                         log("Solo se ha encontrado un agente jugador");
@@ -132,48 +134,60 @@ public class MainAgent extends Agent {
 
                 // Iteramos sobre el número de rondas
                 for (int round = 1; round <= R; round++) {
+                    log("\nIniciando la ronda " + round + "...");
+
                     // Comprobamos si se ha pulsado el botón de pausa
                     checkStop();
 
-                    log("\nIniciando la ronda " + round + "...");
+                    // Comprobamos si se ha eliminado algún jugador
+                    checkRemovedPlayers();
 
                     // Reiniciamos el payoff de la ronda de cada jugador
                     for (Player player : players) {
                         player.setRoundMoney(0);
                     }
 
+                    // Creamos una lista con los IDs de los jugadores
+                    ArrayList<Integer> playerIDs = new ArrayList<>();
+                    for (Player player : players) {
+                        playerIDs.add(player.getID());
+                    }
+
                     // Iteramos sobre cada pareja de jugadores
-                    for (int i = 0; i < players.size() - 1; i++) {
-                        for (int j = i + 1; j < players.size(); j++) {
+                    for (int i = 0; i < playerIDs.size() - 1; i++) {
+                        for (int j = i + 1; j < playerIDs.size(); j++) {
                             // Obtenemos los jugadores, y construímos su mensaje
-                            Player player1 = players.get(i);
-                            Player player2 = players.get(j);
-                            String message = "NewGame#" + i + "#" + j;
+                            Player player1 = getPlayerByID(playerIDs.get(i));
+                            Player player2 = getPlayerByID(playerIDs.get(j));
+                            String message = "NewGame#" + player1.getID() + "#" + player2.getID();
 
                             // Enviamos los mensajes
                             sendMessage(ACLMessage.INFORM, player1.getAID(), message);
                             sendMessage(ACLMessage.INFORM, player2.getAID(), message);
 
-                            log("Mensaje enviado a jugadores con ID " + i + "," + j + ": " + message);
+                            log("Mensaje enviado a jugadores con ID " + player1.getID() + "," + player2.getID() + ": "
+                                    + message);
 
                             // Construímos el mensaje de solicitud de acción
                             message = "Action";
 
                             // Enviamos el mensaje al primer jugador
                             sendMessage(ACLMessage.REQUEST, player1.getAID(), message);
-                            log("Mensaje enviado a jugador con ID " + i + ": " + message);
+                            log("Mensaje enviado a jugador con ID " + player1.getID() + ": " + message);
                             // Esperamos la respuesta
                             ACLMessage reply_player1 = blockingReceive();
-                            log("Mensaje recibido del jugador con ID " + i + ": " + reply_player1.getContent());
+                            log("Mensaje recibido del jugador con ID " + player1.getID() + ": "
+                                    + reply_player1.getContent());
                             // Extraemos del contenido la acción seleccionada
                             String action_player1 = reply_player1.getContent().split("#")[1];
 
                             // Enviamos el mensaje al segundo jugador
                             sendMessage(ACLMessage.REQUEST, player2.getAID(), message);
-                            log("Mensaje enviado a jugador con ID " + j + ": " + message);
+                            log("Mensaje enviado a jugador con ID " + player2.getID() + ": " + message);
                             // Esperamos la respuesta
                             ACLMessage reply_player2 = blockingReceive();
-                            log("Mensaje recibido del jugador con ID " + j + ": " + reply_player2.getContent());
+                            log("Mensaje recibido del jugador con ID " + player2.getID() + ": "
+                                    + reply_player2.getContent());
                             // Extraemos del contenido la acción seleccionada
                             String action_player2 = reply_player2.getContent().split("#")[1];
 
@@ -201,13 +215,15 @@ public class MainAgent extends Agent {
                             }
 
                             // Construímos el mensaje de resultados
-                            message = "Results#" + i + "," + j + "#" + action_player1 + "," + action_player2 + "#"
+                            message = "Results#" + player1.getID() + "," + player2.getID() + "#" + action_player1 + ","
+                                    + action_player2 + "#"
                                     + payoffs[0] + "," + payoffs[1];
 
                             // Enviamos los mensajes
                             sendMessage(ACLMessage.INFORM, player1.getAID(), message);
                             sendMessage(ACLMessage.INFORM, player2.getAID(), message);
-                            log("Mensaje enviado a jugadores con ID " + i + "," + j + ": " + message);
+                            log("Mensaje enviado a jugadores con ID " + player1.getID() + "," + player2.getID() + ": "
+                                    + message);
 
                             // Actualizamos la tabla de los jugadores
                             updatePlayersTable();
@@ -215,25 +231,23 @@ public class MainAgent extends Agent {
                     }
 
                     // Recorremos la lista de jugadores
-                    for (int i = 0; i < players.size(); i++) {
-
-                        // Obtenemos el jugador
-                        Player player = players.get(i);
+                    for (Player player : players) {
 
                         // Aplicamos el índice de inflación a su payoff acumulado
                         player.removeMoney(player.getMoney() * (1 - getInflationRate(round)));
 
                         // Construímos el mensaje de fin de ronda
-                        String message = "RoundOver#" + i + "#" + player.getRoundMoney() + "#" + player.getMoney() + "#"
+                        String message = "RoundOver#" + player.getID() + "#" + player.getRoundMoney() + "#"
+                                + player.getMoney() + "#"
                                 + getInflationRate(round)
                                 + "#" + player.getStocks() + "#" + getIndexValue(round);
 
                         // Enviamos el mensaje
                         sendMessage(ACLMessage.REQUEST, player.getAID(), message);
-                        log("Mensaje enviado a jugador con ID " + i + ": " + message);
+                        log("Mensaje enviado a jugador con ID " + player.getID() + ": " + message);
                         // Esperamos la respuesta
                         ACLMessage reply = blockingReceive();
-                        log("Mensaje recibido del jugador con ID " + i + ": " + reply.getContent());
+                        log("Mensaje recibido del jugador con ID " + player.getID() + ": " + reply.getContent());
 
                         // Extraemos del contenido la acción y la cantidad
                         String action = reply.getContent().split("#")[0];
@@ -260,12 +274,12 @@ public class MainAgent extends Agent {
                         }
 
                         // Construímos el mensaje de contabilidad
-                        message = "Accounting#" + i + "#" + player.getMoney() + "#"
+                        message = "Accounting#" + player.getID() + "#" + player.getMoney() + "#"
                                 + player.getStocks();
 
                         // Enviamos el mensaje
                         sendMessage(ACLMessage.INFORM, player.getAID(), message);
-                        log("Mensaje enviado a jugador con ID " + i + ": " + message);
+                        log("Mensaje enviado a jugador con ID " + player.getID() + ": " + message);
 
                         // Actualizamos la tabla de los jugadores
                         updatePlayersTable();
@@ -275,21 +289,18 @@ public class MainAgent extends Agent {
                 log("\nTorneo finalizado, informando a los jugadores...");
 
                 // Recorremos la lista de jugadores
-                for (int i = 0; i < players.size(); i++) {
-
-                    // Obtenemos el jugador
-                    Player player = players.get(i);
+                for (Player player : players) {
 
                     // Sumamos a su payoff acumulado el valor de venta de sus stocks, aplicando la
                     // comisión de venta
                     player.addMoney((player.getStocks() * getIndexValue(R)) * (1 - F));
 
                     // Construímos el mensaje de fin de torneo
-                    String message = "GameOver#" + i + "#" + player.getMoney();
+                    String message = "GameOver#" + player.getID() + "#" + player.getMoney();
 
                     // Enviamos el mensaje
                     sendMessage(ACLMessage.REQUEST, player.getAID(), message);
-                    log("Mensaje enviado a jugador con ID " + i + ": " + message);
+                    log("Mensaje enviado a jugador con ID " + player.getID() + ": " + message);
 
                     // Actualizamos la tabla de los jugadores
                     updatePlayersTable();
@@ -400,6 +411,18 @@ public class MainAgent extends Agent {
         notify();
     }
 
+    // Método para buscar un jugador por su ID
+    public static Player getPlayerByID(int ID) {
+        // Recorremos la lista de jugadores buscando el ID indicado
+        for (Player player : players) {
+            if (player.getID() == ID) {
+                return player;
+            }
+        }
+        // Si no existe, devolvemos null
+        return null;
+    }
+
     // Método para añadir un jugador a la tabla de los jugadores
     public static void addPlayerToTable(Player player) {
         Platform.runLater(() -> {
@@ -412,6 +435,16 @@ public class MainAgent extends Agent {
         Platform.runLater(() -> {
             controller.updatePlayersTable();
         });
+    }
+
+    // Método para comprobar si hay jugadores que eliminar
+    public void checkRemovedPlayers() {
+        // Recorremos la lista de jugadores para eliminar, y los eliminamos
+        for (Player player : playersToRemove) {
+            players.remove(player);
+        }
+        // Vaciamos la lista de jugadores para eliminar
+        playersToRemove.clear();
     }
 
     // Método para registrar mensajes
